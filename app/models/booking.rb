5bf6b463efd1368b5cfd3f2ce4f11731306ac76d
theodorepgl <limlimtheo96@gmail.com>
2023-10-11 16:57:45 +0800

    Bookings debug

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,8 +3,15 @@ class Booking < ApplicationRecord
   belongs_to :listing
 
   after_create :calculate_duration
+  after_create :calculate_total_price
 
   def calculate_duration
-    self.duration = (check_in. - check_out).to_i    
+    self.duration = (check_in - check_out).to_i
+    self.save    
+  end
+
+  def calculate_total_price
+    self.total_price = duration * listing.price
+    self.save
   end
 end
